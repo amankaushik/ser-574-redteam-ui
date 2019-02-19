51e1dbfb19eaa6c34cb1de122a400f00e98a3655
Divya <dyadamre@asu.edu>
2019-02-18 21:26:04 -0700

    Creating a navigation bar Github analysis US#11 Task#18

--- src/views/GitHub.vue
+++ src/views/GitHub.vue
@@ -1,10 +1,40 @@
 <template>
-
+    <v-flex xs2>
+        <v-navigation-drawer permanent absolute>
+            <v-toolbar flat>
+                <v-list>
+                    <v-list-tile color="success">
+                        <v-list-tile-title class="title">
+                            Github Analysis
+                        </v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list dense class="pt-0">
+                <v-list-tile v-for="path in paths" :key="path.name" :href="path.route">
+                    <v-list-tile-action>
+                        <v-icon>{{ path.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        {{ path.name }}
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+    </v-flex>
 </template>
 
 <script>
     export default {
-        name: "GitHub"
+        name: "GitHub",
+	data() {
+            return {
+                paths: [{name: 'Code Quality', route: '/quality', icon: 'dashboard'},
+                    {name: 'Commits', route: '/comments', icon: 'dashboard'},
+                    {name: 'Code Complexity', route: 'complexity', icon: 'dashboard'}]
+            }
+        }
     }
 </script>
 
---
+++ src/views/complexity.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Complexity"
+    }
+</script>
+
+<style scoped>
+
+</style>
